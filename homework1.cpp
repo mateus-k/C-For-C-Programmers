6378ed49ee2992574e351117a34077cc2258660d
Mateus Tomazini Kinasz <mateuskinasz@gmail.com>
2019-10-26 14:32:13 +0100

    In C++ (not in C), you can pass a variable by reference by passing in a variable. Any modifications to the variable inside the function will also affect the variable here in this scope too.

--- homework1.cpp
+++ homework1.cpp
@@ -13,11 +13,11 @@ using namespace std;
 const int N = 40;
 
 //void sum(int*p, int n, int d[]){
-inline void sum(int*p, int n, int d[]){
+inline void sum(int&p, int n, int d[]){
 	int i;
-	*p = 0;
+	p = 0;
 	for(i = 0; i < n; ++i)
-	*p = *p + d[i];
+	p = p + d[i];
 }
 
 int main(){
@@ -29,7 +29,7 @@ int main(){
 	for(i = 0; i < N; ++i)
 		data[i] = i;
 	
-	sum(&accum, N, data);
+	sum(accum, N, data);
 
 //	printf("sum is %d\n", accum);
 	cout << "sum is " << accum << "\n";
